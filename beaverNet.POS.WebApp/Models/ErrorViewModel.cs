dc41c7437238d4bbb6adc2df248d6d8dcb15dc68
Julio Cesar Melchor Pinto <pirate.store@hotmail.com>
2020-03-22 19:26:31 -0700

    Add project files.

---
+++ beaverNet.POS.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace beaverNet.POS.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
