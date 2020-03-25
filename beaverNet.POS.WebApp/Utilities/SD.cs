15d799f67456f33c50b59037a4b67dd4264b3ffb
JulioMelchorPinto <julio.melchor@ciencias.unam.mx>
2020-03-25 00:15:41 -0700

    add helpers to workspace

--- beaverNet.POS.WebApp/Utilities/SD.cs
+++ beaverNet.POS.WebApp/Utilities/SD.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace beaverNet.POS.WebApp.Utility
+namespace beaverNet.POS.WebApp.Utilities
 {
     public static class SD
     {
